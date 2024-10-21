d8c8f27dd7c9dc30194b9d750f8cc2ec9530ec0c
moussa23m <moussaelsahnouni@gmail.com>
2024-10-21 08:16:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿document.getElementById('uploadForm').addEventListener('submit', function(e) {
+  e.preventDefault(); // لمنع إعادة تحميل الصفحة
+
+  // التحقق من تحميل جميع الملفات المطلوبة
+  const file1 = document.getElementById('file1').files[0];
+  const file2 = document.getElementById('file2').files[0];
+  const file3 = document.getElementById('file3').files[0];
+
+  if (!file1 || !file2 || !file3) {
+    alert("⚠️ الرجاء تحميل جميع الملفات المطلوبة.");
+    return;
+  }
+
+  alert("✅ تم إرسال الملفات بنجاح!");
+});
